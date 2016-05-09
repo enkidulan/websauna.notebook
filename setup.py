ec4c3fceaae648596eafac0fce119b4e15ecca00
Maksym Shalenyi <enkidulan@gmail.com>
2016-05-09 13:58:24 +0300

    added ipython notebook dependencies

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setup(name='websauna.notebook',
       description='notebook',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
       author='',
       author_email='',
       url='',
@@ -28,13 +28,18 @@ setup(name='websauna.notebook',
       include_package_data=True,
       zip_safe=False,
       test_suite='websauna.notebook',
-      install_requires=['websauna'],
+      install_requires=[
+          'websauna',
+          'PasteDeploy',
+          'ipython[notebook]<4',
+          'pyramid_notebook>=0.1.6',
+      ],
       extras_require={
-        # Dependencies for running test suite
-        'test': ['websauna[test]'],
+          # Dependencies for running test suite
+          'test': ['websauna[test]'],
 
-        # Dependencies to make releases
-        'dev': ['websauna[dev]'],
+          # Dependencies to make releases
+          'dev': ['websauna[dev]'],
       },
 
       # Define where this application starts as referred by WSGI web servers
